f42ce4e0e48ba9d9005dc189ba9c4bfd40bb571b
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-06-08 09:12:28 +0630

    session updated

--- app/Helpers/SessionHelper.php
+++ app/Helpers/SessionHelper.php
@@ -17,7 +17,7 @@ class SessionHelper
 
         if ($currentTime >= '04:00:00' && $currentTime <= '12:01:00') {
             return 'morning';
-        } elseif ($currentTime >= '12:01:01' && $currentTime <= '16:30:00') {
+        } elseif ($currentTime >= '12:01:01' && $currentTime <= '23:30:00') {
             return 'evening';
         } else {
             return 'closed';
